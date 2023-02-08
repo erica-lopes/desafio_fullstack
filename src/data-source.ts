00fe7e27e0a436eb9ab8c87392c074da87d885e2
Erica Lopes <ericalopes@outlook.com>
2023-02-08 10:43:44 -0300

    fix: correção de configurações

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,10 +12,6 @@ const AppDataSource = new DataSource(
     : {
         type: "postgres",
         url: process.env.DATABASE_URL,
-        ssl:
-          process.env.NODE_ENV === "production"
-            ? { rejectUnauthorized: false }
-            : false,
         synchronize: false,
         logging: true,
         entities:
