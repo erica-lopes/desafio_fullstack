d0b27ab228f4968c09954c8507cb92aca5027f41
Erica Lopes <ericalopes@outlook.com>
2023-02-12 16:23:31 -0300

    fix: configurações database

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,17 +11,15 @@ const AppDataSource = new DataSource(
       }
     : {
         type: "postgres",
-        url: process.env.DATABASE_URL,
+        host: process.env.DB_HOST,
+        port: 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
         synchronize: false,
         logging: true,
-        entities:
-          process.env.NODE_ENV === "production"
-            ? ["dist/src/entities/*.js"]
-            : ["src/entities/*.ts"],
-        migrations:
-          process.env.NODE_ENV === "production"
-            ? ["dist/src/migrations/*.js"]
-            : ["src/migrations/*.ts"],
+        entities: ["src/entities/*.ts"],
+        migrations: ["src/migrations/*.ts"],
       }
 );
 
